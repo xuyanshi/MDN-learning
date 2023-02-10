9047ea4e82c92022ca8d5238dc88a71c80e9f054
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-02-10 16:55:52 +0800

    change user

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -8,3 +8,27 @@ myImage.onclick = function () {
         myImage.setAttribute('src', './images/genshin-akihabara.jpg');
     }
 }
+
+let myButton = document.querySelector('button')
+let myHeading = document.querySelector('h1')
+
+function setUserName() {
+    let myName = prompt('Input your name:');
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Hello, ' + myName;
+    }
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Hello, ' + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName();
+}
