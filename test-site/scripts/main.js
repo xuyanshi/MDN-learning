e9d1f5b39ad8410ee7c7de535cdd1e7f0c9b6f59
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-02-10 16:49:18 +0800

    support image change

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -1,2 +1,10 @@
-let myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello!';
+let myImage = document.querySelector('img')
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === './images/genshin-akihabara.jpg') {
+        myImage.setAttribute('src', './images/tachiiri_kinshi.PNG');
+    } else {
+        myImage.setAttribute('src', './images/genshin-akihabara.jpg');
+    }
+}
