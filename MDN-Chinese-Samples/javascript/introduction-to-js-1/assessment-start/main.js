64be9e3e499485ea9b1671e852e8463174cfaf2f
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-03-03 14:38:44 +0800

    add JavaScript

---
+++ MDN-Chinese-Samples/javascript/introduction-to-js-1/assessment-start/main.js
@@ -0,0 +1,7 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
